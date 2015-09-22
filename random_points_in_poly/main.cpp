5b5cac26dbd2429e6620f48277ba55fe050b4c05
sw897 <sunwei.r@gmail.com>
2015-09-22 18:25:31 +0800

    add polygon simplify

--- random_points_in_poly/main.cpp
+++ random_points_in_poly/main.cpp
@@ -1,6 +1,6 @@
 #include "poly2tri.h"
 #include "common/triangle_utils.h"
-
+#include "clipper.hpp"
 #include "shapefil.h"
 
 #include <iostream>
@@ -9,17 +9,45 @@
 #include <string>
 #include <vector>
 
+static const double COORDINATE_PRECISION = 10000000.;
+
+using namespace ClipperLib;
+using namespace p2t;
+using namespace std;
+
+void print_triangle(vector<Triangle*> triangles)
+{
+    int icount = 0;
+    // print
+    for (auto it = triangles.begin(); it != triangles.end(); ++it)
+    {
+        if (icount != 0)
+            cout << "," << endl;
+        cout << "    { \"type\": \"Feature\"," << endl;
+
+        Triangle& tri = **it;
+        cout << "      \"geometry\": {\"type\": \"Polygon\",\"coordinates\": [[";
+        Point& pt1 = *tri.GetPoint(0);
+        Point& pt2 = *tri.GetPoint(1);
+        Point& pt3 = *tri.GetPoint(2);
+        cout << "[" << pt1.x / COORDINATE_PRECISION << ", " << pt1.y / COORDINATE_PRECISION << "], ";
+        cout << "[" << pt2.x / COORDINATE_PRECISION << ", " << pt2.y / COORDINATE_PRECISION << "], ";
+        cout << "[" << pt3.x / COORDINATE_PRECISION << ", " << pt3.y / COORDINATE_PRECISION << "], ";
+        cout << "[" << pt1.x / COORDINATE_PRECISION << ", " << pt1.y / COORDINATE_PRECISION << "]";
+        cout << "]]}," << endl;
+        cout << "      \"properties\": {\"area\": \"kaka\"}" << endl;
+        icount++;
+
+        cout << "    }";
+    }
+}
 
 int main(int argc, char ** argv)
 {
-
-    using namespace p2t;
-    using namespace std;
     //random_points_in_triangle(110., 35., 120., 35., 115., 40., 10000, "out.csv");
 
     string adm_shp = "/download/china_adm/china_adm_1.shp";
-    int		nShapeType, nEntities, i, iPart, bValidate = 1,nInvalidCount=0;
-    const char 	*pszPlus;
+    int		nShapeType, nEntities, i, iPart, nInvalidCount=0;
     double 	adfMinBound[4], adfMaxBound[4];
 
     SHPHandle	hSHP = SHPOpen( adm_shp.c_str(), "rb" );
@@ -67,26 +95,15 @@ int main(int argc, char ** argv)
             break;
         }
 
-        if( psShape->bMeasureIsUsed )
-            printf( "\nShape:%d (%s)  nVertices=%d, nParts=%d\n"
-                    "  Bounds:(%.15g,%.15g, %.15g, %.15g)\n"
-                    "      to (%.15g,%.15g, %.15g, %.15g)\n",
-                    i, SHPTypeName(psShape->nSHPType),
-                    psShape->nVertices, psShape->nParts,
-                    psShape->dfXMin, psShape->dfYMin,
-                    psShape->dfZMin, psShape->dfMMin,
-                    psShape->dfXMax, psShape->dfYMax,
-                    psShape->dfZMax, psShape->dfMMax );
-        else
-            printf( "\nShape:%d (%s)  nVertices=%d, nParts=%d\n"
-                    "  Bounds:(%.15g,%.15g, %.15g)\n"
-                    "      to (%.15g,%.15g, %.15g)\n",
-                    i, SHPTypeName(psShape->nSHPType),
-                    psShape->nVertices, psShape->nParts,
-                    psShape->dfXMin, psShape->dfYMin,
-                    psShape->dfZMin,
-                    psShape->dfXMax, psShape->dfYMax,
-                    psShape->dfZMax );
+        printf( "\nShape:%d (%s)  nVertices=%d, nParts=%d\n"
+                "  Bounds:(%.15g,%.15g, %.15g)\n"
+                "      to (%.15g,%.15g, %.15g)\n",
+                i, SHPTypeName(psShape->nSHPType),
+                psShape->nVertices, psShape->nParts,
+                psShape->dfXMin, psShape->dfYMin,
+                psShape->dfZMin,
+                psShape->dfXMax, psShape->dfYMax,
+                psShape->dfZMax );
 
         if( psShape->nParts > 0 && psShape->panPartStart[0] != 0 )
         {
@@ -94,83 +111,107 @@ int main(int argc, char ** argv)
                      psShape->panPartStart[0] );
         }
 
-        //
-        vector<vector<Point*>> polylines;
+        int nAltered = SHPRewindObject( hSHP, psShape );
+        if( nAltered > 0 )
+        {
+            printf( "  %d rings wound in the wrong direction.\n", nAltered );
+            nInvalidCount++;
+        }
+
         // redirect cout
         stringstream sstr;
         sstr << i;
-        string out_file = "tri_vertex_" + sstr.str() + ".csv";
+        string out_file = "tri_vertex_" + sstr.str() + ".geojson";
         ofstream output(out_file);
         streambuf* fileBuf = output.rdbuf();
         streambuf* coutBuf = cout.rdbuf();
         cout.rdbuf(fileBuf);
 
+        cout << "{ \"type\": \"FeatureCollection\"," << endl;
+        cout << "  \"features\": [" << endl;
+
+        double total_area = 0.0;
+        int icount = 0;
         for ( iPart = 0; iPart < psShape->nParts; iPart++)
         {
-            vector<Point*> polyline;
+            // 1. simplify polygon using clipper
+            Path path;
+            Paths out_paths;
             int start = psShape->panPartStart[iPart];
             int end = psShape->nVertices;
             if (iPart < psShape->nParts - 1)
                 end = psShape->panPartStart[iPart + 1];
 
-            // remove the last point, because it is the same with the first one
-            for( j = start; j < end - 1; j++ )
+            for (j = start; j < end; j++)
             {
-                const char	*pszPartType = "";
+                long long x = cInt(psShape->padfX[j]*COORDINATE_PRECISION);
+                long long y = cInt(psShape->padfY[j]*COORDINATE_PRECISION);
+                path.push_back(IntPoint(cInt(x), cInt(y)));
+            }
+            SimplifyPolygon(path, out_paths);
 
-                if( j == 0 && psShape->nParts > 0 )
-                    pszPartType = SHPPartTypeName( psShape->panPartType[0] );
-
-                if (polyline.size() > 0)
+            // 2.trianglate polygon using poly2tri
+            for (auto it = out_paths.begin(); it != out_paths.end(); ++it)
+            {
+                if (it->size() > 2)
                 {
-                    Point& pt = *polyline[polyline.size()-1];
-                    if (pt.x == psShape->padfX[j] && pt.y == psShape->padfY[j])
-                        continue;
+                    if(icount != 0)
+                        cout << "," << endl;
+                    else
+                        cout << endl;
+                    icount++;
+                    vector<Point*> polyline;
+                    for (auto it2 = it->begin(); it2 != it->end(); ++it2)
+                    {
+                        polyline.push_back(new Point(it2->X, it2->Y));
+                    }
+                    // trianglate
+                    CDT* cdt = new CDT(polyline);
+                    cdt->Triangulate();
+                    vector<Triangle*> triangles = cdt->GetTriangles();
+                    print_triangle(triangles);
+
+                    // claculate area
+                    for (auto it = triangles.begin(); it != triangles.end(); ++it)
+                    {
+                        Triangle& tri = **it;
+                        double area = triangle_area(tri);
+                        if (area < 0)
+                        {
+                            printf("area < 0, %lf \n", area);
+                            area = std::abs(area);
+                        }
+                        total_area += area;
+                    }
+
+                    // clear
+                    delete cdt;
+                    for(auto it = polyline.begin(); it != polyline.end(); ++it)
+                    {
+                        delete *it;
+                    }
+                    polyline.clear();
                 }
-                polyline.push_back(new Point(psShape->padfX[j], psShape->padfY[j]));
             }
-            polylines.push_back(polyline);
 
-            // trianglate
-            CDT* cdt = new CDT(polyline);
-            cdt->Triangulate();
-            vector<Triangle*> triangles = cdt->GetTriangles();
-            list<Triangle*> map = cdt->GetMap();
+//            vector<Point*> polyline;
+//            if(start < end - 2)
+//            {
+//                // skip the last point
+//                for( j = start; j < end - 1; j++ )
+//                {
+//                    double x = psShape->padfX[j]*COORDINATE_PRECISION;
+//                    double y = psShape->padfY[j]*COORDINATE_PRECISION;
+//                    polyline.push_back(new Point(x, y));
+//                }
+//            }
 
-            // print
-            int icount = 0;
-            for (auto it = triangles.begin(); it != triangles.end(); ++it)
-            {
-                Triangle& tri = **it;
-                for (int iVertex = 0; iVertex < 3; ++iVertex)
-                {
-                    Point& pt = *tri.GetPoint(iVertex);
-                    cout << pt.x << "," << pt.y << endl;
-                    printf("%d, ", icount*3 + iVertex);
-                }
-                icount++;
-            }
-        }
-        if( bValidate )
-        {
-            int nAltered = SHPRewindObject( hSHP, psShape );
-
-            if( nAltered > 0 )
-            {
-                printf( "  %d rings wound in the wrong direction.\n",
-                        nAltered );
-                nInvalidCount++;
-            }
         }
 
-        // free
-        for (auto it = polylines.begin(); it != polylines.end(); ++it)
-        {
-            for (auto it2 = it->begin(); it2 != it->end(); ++it2)
-            {
-                delete *it2;
-            }
-        }
+        cout << "  ]" << endl;
+        cout << "}" << endl;
+
+        printf(" total area is %lf \n", total_area);
 
         // resume cout
         output.flush();
@@ -178,15 +219,9 @@ int main(int argc, char ** argv)
         std::cout.rdbuf(coutBuf);
 
         SHPDestroyObject( psShape );
-        break;
     }
 
     SHPClose( hSHP );
-
-    if( bValidate )
-    {
-        printf( "%d object has invalid ring orderings.\n", nInvalidCount );
-    }
 
     return 0;
 }
--- random_points_in_poly/random_points_in_poly.pro
+++ random_points_in_poly/random_points_in_poly.pro
@@ -14,13 +14,22 @@ DESTDIR = $$PWD/bin
 
 TEMPLATE = app
 
+#poly2tri
 INCLUDEPATH += $$PWD/../../poly2tri/poly2tri
 DEPENDPATH += $$PWD/../../poly2tri/poly2tri
 LIBS += -L$$PWD/../../poly2tri/poly2tri/lib -lpoly2tri
 
+#clipper
+INCLUDEPATH += $$PWD/../../clipper/clipper
+DEPENDPATH += $$PWD/../../clipper/clipper
+LIBS += -L$$PWD/../../clipper/clipper/lib -lclipper
+
+#shapelib
 INCLUDEPATH += /sztt-build/3rdparty/shapelib
 DEPENDPATH += /sztt-build/3rdparty/shapelib
 LIBS += -L/sztt-build/3rdparty/shapelib/win32 -lshapelib_i
 
 SOURCES += \
     main.cpp
+
+HEADERS +=
